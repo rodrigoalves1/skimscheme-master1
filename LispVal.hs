10d89668b11d2ba3275d36cdd8318313b9d43ce0
rodrigoalves1 <raan@cin.ufpe.br>
2015-06-05 12:58:20 -0300

    Create LispVal.hs

---
+++ LispVal.hs
@@ -0,0 +1,24 @@
+module LispVal (LispVal(Atom, List, DottedList, Number, String, Bool, Error, Native, Comment, Test)) where
+
+import Data.Map as Map
+-----------------------------------------------------------
+--                    BASIC DATATYPES                    --
+-----------------------------------------------------------
+{- 
+In Lisp, the data types representing code structures are the same as the
+data types representing values. This somewhat simplifies the
+construction of an interpreter. For other languages, one would use
+different datatypes to represent structures that appear in the code
+(statements, expressions, declarations, etc.) and the data that their
+evaluation produces.
+-}
+data LispVal = Atom String
+  | List [ LispVal ]
+  | DottedList [ LispVal ] LispVal
+  | Number Integer
+  | String String 
+  | Bool Bool
+  | Error String
+  | Native ([LispVal] -> LispVal)
+  | Comment
+  | Test LispVal (Map String LispVal)
